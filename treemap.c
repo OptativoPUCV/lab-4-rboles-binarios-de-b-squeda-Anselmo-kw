1ccbbfecf217d686da367e3cd72f4198bfebfcfc
Your Name <you@example.com>
2025-05-01 16:46:44 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -172,7 +172,7 @@ minimum (para hacerlo puede usar la misma función *removeNode*). */
     /*node->pair->key = hijoDer->pair->key;
     node->pair->value = hijoDer->pair->value;*/
 
-    /*
+    
     if(node->left != NULL && node->right != NULL)
     {
         TreeNode * hijoDer = minimum(node->right);
@@ -184,7 +184,7 @@ minimum (para hacerlo puede usar la misma función *removeNode*). */
         node->pair->value = hijoDer->pair->value;
 
         removeNode(tree, hijoDer);
-    }*/
+    }
 
 }
 
