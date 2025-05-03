c0e03ce67ebc991ecb6c7ab6de4d5839a2365921
Your Name <you@example.com>
2025-05-02 22:19:33 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -234,8 +234,10 @@ o igual a key. Para implementarla puede realizar una búsqueda normal y usar un 
 a nodo auxiliar ub_node que vaya guardando el nodo con la menor clave *mayor o igual a 
 key*. Finalmente retorne el par del nodo ub\_node. */
 Pair * upperBound(TreeMap * tree, void* key) {
+    if(tree == NULL || tree->root == NULL) return NULL;
     TreeNode * aux = tree->root;
     TreeNode * guardado = aux;
+
 
     while(aux != NULL)
     {
@@ -261,7 +263,6 @@ Pair * upperBound(TreeMap * tree, void* key) {
         }
 
     }
-
 
     return guardado->pair;
 }
